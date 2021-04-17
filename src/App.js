e01acba44065924c1a98d591340755be4c91aaf4
priya-002 <priya.md0002@gmail.com>
2021-04-17 19:48:50 +0530

    background-image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Navbar from "./components/navbar/Navbar";
 import "./App.css"
+import Intro from "./components/intro/Intro";
+
 
 
 
@@ -7,6 +9,7 @@ function App() {
   return (
     <div className="App">
      <Navbar />
+     <Intro />
     </div>
   );
 }
---
+++ src/components/intro/Intro.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import bg from '../../assets/bg.png'
+import "./intro.css"
+
+const intro = () => {
+    return (
+        <div>
+            <img className="intro__image" src={bg} alt=""/>
+        </div>
+    )
+}
+
+export default intro
