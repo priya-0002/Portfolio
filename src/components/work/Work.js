0c616d35503d94af5dd617cda784dbd57ffb5bcb
priya-002 <priya.md0002@gmail.com>
2021-04-23 19:18:26 +0530

    work css

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import {useState} from 'react';
+import "./work.css"
 
 
 const Work = (props) => {
     const{title,description}=props;
     const[show,setShow]=useState(false);
+   
 
     
     return (
         <div className="work__subheading">
          
-           {show?<p>{description}</p>:null}
-           <button onClick={()=>setShow(!show)}>nnnnnn</button>
+          <div >
+           <button className="work__button" onClick={()=>setShow(!show)}>{title}</button>
+           </div>
+
+        <div>
+           {show?<p className="work__para">{description}</p>:null}
+           </div>
             
              </div>
     )
--- src/components/work/Workparent.js
+++ src/components/work/Workparent.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import Work from './Work'
+import "./work.css"
 
 const Workparent = () => {
-  let url="innoovatum.com"
+  
     return (
         <div>
               <h1 className="work__heading">
                 Experience
-            </h1>
-          <Work title="Innoovatum.com"/>
-          <Work description="Innoovatum is a software company which believes in practical engineering; having its own search engine, which is one platform to grab engineering related resources. I've worked there for 1 year as a junior web developer on the search engine."/>
+         </h1>
+         
+              
+            
+          <Work title="Innoovatum.com"description="Innoovatum is a software company which believes in practical engineering; having its own search engine, which is one platform to grab engineering related resources. I've worked there for 1 year as a junior web developer on the search engine."/>
+          <Work title="Innoovatum.com"description="Innoovatum is a software company which believes in practical engineering; having its own search engine, which is one platform to grab engineering related resources. I've worked there for 1 year as a junior web developer on the search engine."/>
+          <Work title="Innoovatum.com"description="Innoovatum is a software company which believes in practical engineering; having its own search engine, which is one platform to grab engineering related resources. I've worked there for 1 year as a junior web developer on the search engine."/>
+          
           
         </div>
     )
