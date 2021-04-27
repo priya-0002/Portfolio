be1ea6ff50520f814c2e8c8fce6c6759effde1e1
priya-002 <priya.md0002@gmail.com>
2021-04-27 21:48:19 +0530

    contact ui

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -9,10 +9,17 @@ const Contact = () => {
     return (
         <div>
            <h1>Contact </h1>
-            <div className="icons">
-               <a href="">{gmail}</a> 
-            </div>
+            
+          
+               
+            <div className="icon"   >
 
+           <a href=""> <img className="t"src={twitter} alt="twitter logo"/></a>
+              <a href="https://github.com/priya-0002/"> <img className="g" src={git} alt="github logo"/></a>
+               <a href="https://www.linkedin.com/in/priya-d-41879a1a4/"> <img className="l"src={linkedin} alt="linked logo"/></a>
+               
+
+               </div>
         </div>
     )
 }
