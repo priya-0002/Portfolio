ced2087e36435f39347af73c352dc8c94081a355
priya-002 <priya.md0002@gmail.com>
2021-04-29 21:24:38 +0530

    contact ui

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import priya from "../../assets/priya.png"
+
 
 
 const Project = (props) => {
-    const{name}=props;
+    const{name,img,head,body}=props;
     return (
         <div>
               <div className={name}>
             
-            <img className="project__profile"src={priya} alt=""/>
+            <img className="project__profile"src={img} alt=""/>
+            
+
         
         <div className="project__text">
-            <h1>Projects</h1>
-            <p className="project__para"> I keep interest in learning new technologies.</p>
+            <h1>{head}</h1>
+            <p className="project__para"> {body}</p>
         </div>
     </div>
   
--- src/components/projects/Projectparent.js
+++ src/components/projects/Projectparent.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Project from './Project'
 import "./project.css"
+import css from "../../assets/css.png"
+import netflix from "../../assets/netflix.png"
+import blog from "../../assets/blog.png"
 
 const Projectparent = () => {
     return (
         <div>
-           <Project name="project__parent" />
-           <Project name="project__changes"/>
-           <Project name="project__parent" />
+           <Project name="project__parent" img={css} head="Landing Page "  body="I have used advance css to style my front page"/>
+           <Project name="project__changes" img={netflix} head="Netflix Landing Page"body="I tried cloning netflix landing page"/>
+           <Project name="project__parent" img={blog} head="Blog Application"body="I have built an crud application using react js"/>
         </div>
     )
 }
