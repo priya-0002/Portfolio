81bb2d5b5c6adea8beb7e2f8f7720646df8029a8
Prajwal <61734018+prajwalr308@users.noreply.github.com>
2021-08-13 21:57:57 +0530

    project changes

--- src/components/projects/Projectparent.js
+++ src/components/projects/Projectparent.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Project from './Project'
 import "./project.css"
-import css from "../../assets/css.png"
+import krishi from "../../assets/krishiseva.png"
 import netflix from "../../assets/netflix.png"
-import blog from "../../assets/blog.png"
+import covid from "../../assets/covid.png"
 
 const Projectparent = () => {
     return (
@@ -12,9 +12,9 @@ const Projectparent = () => {
             <h1 className="" style={{display: 'grid',placeItems:"center",marginTop:"20px"}}> Projects</h1>
            
             <br/>
-           <Project name="project__parent" img={css} head="Landing Page "  body="I have used advance css to style my front page"/> <br/> <br/>
+           <Project name="project__parent" img={krishi} head="krishi seva "  body="I have used React to build krishi seva website"/> <br/> <br/>
            <Project name="project__changes" img={netflix} head="Netflix Landing Page"body="I tried cloning netflix landing page"/> <br/> <br/>
-           <Project name="project__parent" img={blog} head="Blog Application"body="I have built an crud application using react js"/>
+           <Project name="project__parent" img={covid} head="Covid tracker"body="I have built covid tracker using disease.sh api "/>
         </div>
     )
 }
